d6a61f3be52bff644d0601c1d6b9d523c06dac26
Swapnil Parekh <swapnilbp100@gmail.com>
2019-12-25 00:03:21 +0530

    nc

--- pump.py
+++ pump.py
@@ -8,6 +8,8 @@ def get_pump(
     mel = pumpp.feature.Mel(
         name='mel', sr=sr,
         n_mels=n_mels,
+        n_fft=2048,
+        hop_length=512,
         log=True, conv='tf')
 
     return pumpp.Pump(mel)
